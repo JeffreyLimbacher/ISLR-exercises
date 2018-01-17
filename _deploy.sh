51aabe7eccddd2b48caac356f35c4b8ff662d626
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2018-01-16 17:43:09 -0800

    get rid of command error in deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -1,9 +1,6 @@
 #credit: https://bookdown.org/yihui/bookdown/github.html
 
 # clone the repository to the book-output directory
-if [! -d "book-output"]; then
-	mkdir book-output
-fi
 git clone -b gh-pages \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
   book-output
